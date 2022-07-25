6466f74010e6039ddef57f6f1c4a1e1d0e729bb0
xpcn2015 <76277169+xpcn2015@users.noreply.github.com>
2022-07-25 19:50:13 +0700

    import from project

---
+++ src/index.ts
@@ -0,0 +1,106 @@
+import { Request } from "express";
+import formidable from "formidable";
+import { unlink } from "fs";
+
+let tempFiles: string[] = []
+const form = new formidable.IncomingForm({ uploadDir: "./temp", keepExtensions: true, multiples: true });
+
+/**
+ * 
+ * @param {Request} req request from express
+ * @returns Promise of Formidable fields and files, or undefined
+ */
+export function parseFormFiles(req: Request) {
+    return new Promise<{ fields: formidable.Fields, files: formidable.Files }>((resolve, rejects) => {
+        form.parse(req, async (err, fields, files) => {
+            if (err) rejects(err)
+            tempFiles = getEntriesFilePath(files)
+            resolve({ fields, files });
+        });
+    })
+}
+
+export function parseFormImage(req: Request, onError?: (message: string) => void, bodyName = "images") {
+    return new Promise<{ fields: formidable.Fields, files?: fileInfo[] }>((resolve, rejects) => {
+        form.parse(req, async (err, fields, files) => {
+            if (err) rejects(`Failed to begin parse image => \n${err} `)
+
+            tempFiles = getEntriesFilePath(files)
+            const filesInfo = getFilesInfo({
+                file: files[bodyName],
+                allowedType: ["image/jpeg", "image/jpg", "image/png"],
+                onError
+            })
+            // if (!filesInfo) rejects("failed to parse image files")
+            resolve({ fields, files: filesInfo });
+        });
+    })
+}
+
+
+export function cleanupFormFile(onError?: (message: string) => void) {
+    tempFiles.forEach(file => {
+        unlink(file, (err) => {
+            if (err && onError) onError(`Cannot delete temporary uploaded file => \n${err} `);
+        })
+    })
+    tempFiles = []
+}
+
+
+
+export function getFilesInfo({ file, allowedType, onError }: getFilepathProps) {
+    const output: fileInfo[] = []
+    if (!file) {
+        onError ? onError(`Empty file input`) : null
+        return
+    }
+
+    const assignOutput = (f: formidable.File) => {
+        const fileType = f.mimetype
+        if (!fileType) {
+            onError ? onError(`Cannot detect file type.`) : null
+            return
+        }
+
+        if (allowedType.findIndex((el) => el === fileType) === -1) {
+            onError ? onError(`Invalid upload file type: \`${fileType}\``) : null
+            return
+        }
+        const fileName = f.originalFilename === null ? "" : f.originalFilename
+        output.push({ filePath: f.filepath, fileType: fileType.split('/')[1], fileName: fileName })
+    }
+
+    Array.isArray(file) ? file.map(assignOutput) : assignOutput(file)
+
+    if (output.length < 1) {
+        onError ? onError(`No file`) : null
+        return
+    }
+    return output
+
+
+}
+
+function getEntriesFilePath(files: formidable.Files) {
+    const output: string[] = []
+    Object.entries(files).forEach(file => {
+        if (Array.isArray(file[1]))
+            file[1].forEach(f => output.push(f.filepath))
+        else
+            output.push(file[1].filepath)
+    })
+    return output
+}
+type getFilepathProps = {
+    file: formidable.File | formidable.File[] | undefined,
+    allowedType: string[],
+    onError?: (message: string) => void
+
+}
+type fileInfo = {
+    filePath: string,
+    fileType: string,
+    fileName: string
+
+}
